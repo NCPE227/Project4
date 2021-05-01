3e22e392612ac8228288bb139b6dfcdd99254f47
NCPE227 <61167576+NCPE227@users.noreply.github.com>
2021-05-01 12:29:01 -0400

    Create main.cpp
    main file for creating a shell

---
+++ main.cpp
@@ -0,0 +1,20 @@
+//-----------------------------------------------
+// Authors: Noah Perry, Evan Courtwright
+// Assignment: Project 4: Create a Simple Shell
+//-----------------------------------------------
+
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <map>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main()
+{
+  
+}
